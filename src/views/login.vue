2681003857cf4f7f36a0f5237f492a6cdb3f18e6
HingLo <13568939633@163.com>
2019-04-17 16:14:49 +0800

    1: 登陆页面修改,添加背景图与位置修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,70 +1,90 @@
 <template>
-    <div id="login">
-        <h1> PERMISSION
-            <br>Management System
-        </h1><br>
-        <Form :model="formInline" :rules="ruleInline" inline ref="formInline">
-            <FormItem prop="username">
-                <i-input class="ipt" placeholder="Username" type="text" v-model="formInline.username">
-                    <Icon slot="prepend" type="ios-person-outline"></Icon>
-                </i-input>
-            </FormItem>
-            <br><br>
-            <FormItem prop="password">
-                <i-input class="ipt" placeholder="Password" type="password" v-model="formInline.password">
-                    <Icon slot="prepend" type="ios-locked-outline"></Icon>
-                </i-input>
-            </FormItem>
-            <br>
-            <FormItem>
-                <Button @click="handleSubmit('formInline')" class="ipt" type="primary">login</Button>
-            </FormItem>
-        </Form>
+    <div class="background">
+        <div id="login">
+            <Card>
+                <p slot="title">欢迎登陆PermissionAdmin系统</p>
+                <Form :model="formInline" :rules="ruleInline" inline ref="formInline">
+                    <FormItem prop="username">
+                        <Input class="ipt" placeholder="Username" type="text" v-model="formInline.username">
+                            <Icon slot="prepend" type="ios-person-outline"></Icon>
+                        </Input>
+                    </FormItem>
+                    <br><br>
+                    <FormItem prop="password">
+                        <Input class="ipt" placeholder="Password" type="password" v-model="formInline.password">
+                            <Icon type="md-person" slot="prepend"/>
+                        </Input>
+                    </FormItem>
+                    <br>
+                    <FormItem>
+                        <Button @click="handleSubmit('formInline')" class="ipt" type="primary">login</Button>
+                    </FormItem>
+                </Form>
+            </Card>
+        </div>
     </div>
 </template>
 
 <script>
-  export default {
-    name: 'login',
-    data() {
-      return {
-        formInline: {
-          username: '',
-          password: ''
+    export default {
+        name: 'login',
+        data() {
+            return {
+                formInline: {
+                    username: '',
+                    password: ''
+                },
+                ruleInline: {
+                    username: [
+                        {required: true, message: '请输入登陆的用户名', trigger: 'blur'}
+                    ],
+                    password: [
+                        {required: true, message: '请输入登陆密码', trigger: 'blur'},
+                        {
+                            type: 'string',
+                            min: 6,
+                            message: 'The password length cannot be less than 6 bits',
+                            trigger: 'blur'
+                        }
+                    ]
+                }
+            }
         },
-        ruleInline: {
-          username: [
-            {required: true, message: 'Please fill in the user name', trigger: 'blur'}
-          ],
-          password: [
-            {required: true, message: 'Please fill in the password.', trigger: 'blur'},
-            {type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur'}
-          ]
+        methods: {
+            handleSubmit(name) {
+                let vm = this
+                this.$refs[name].validate((valid) => {
+                    if (valid) {
+                        this.$emit('login', vm.formInline)
+                    } else {
+                        this.$Message.error('请认真填写登录信息!')
+                    }
+                })
+            }
         }
-      }
-    },
-    methods: {
-      handleSubmit(name) {
-        let vm = this
-        this.$refs[name].validate((valid) => {
-          if (valid) {
-            this.$emit('login', vm.formInline)
-          } else {
-            this.$Message.error('请认真填写登录信息!')
-          }
-        })
-      }
     }
-  }
 </script>
 
 <style scoped>
     #login {
-        padding-top: 10%;
+        margin-top: 10%;
+        right: 5%;
         text-align: center;
+        width: 300px;
+        position: absolute;
+    }
+
+    /*设置背景图片全部沾满*/
+    .background {
+        background-image: url(../assets/login-background.jpg);
+        background-size: cover;
+        background-position: center center;
+        position: fixed;
+        height: 100%;
+        width: 100%;
     }
 
     .ipt {
-        width: 300px
+        width: 250px;
     }
 </style>
